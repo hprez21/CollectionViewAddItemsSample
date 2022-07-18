3d319f7525394907cea6df7e8e64f44914937752
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2022-07-18 11:24:22 -0500

    Add project files.

---
+++ CollectionViewAddItemsSample/CollectionViewAddItemsSample/MainPageModel.cs
@@ -0,0 +1,27 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CollectionViewAddItemsSample
+{
+     [AddINotifyPropertyChangedInterface]
+     public class MainPageModel
+     {
+          public ObservableCollection<Person> People { get; set; } =
+               new ObservableCollection<Person>();
+          public string NameToAdd { get; set; }
+          public ICommand AddCommand =>
+               new Command(() =>
+               {
+                    People.Add(
+                         new Person
+                         {
+                              Name = NameToAdd
+                         });
+               });
+     }
+}
